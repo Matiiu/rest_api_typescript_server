ea1352976192cb50c6c23bf42def6f5d3b99b0ae
Matiiu <mateotaveraramirez@gmail.com>
2024-04-25 10:29:35 -0500

    change: connection title is changed

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import colors from 'colors';
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-	console.log(colors.bgCyan.bold(`rest api en el puerto ${PORT}`));
+	console.log(colors.bgCyan.bold(`Rest API on port ${PORT}`));
 });
