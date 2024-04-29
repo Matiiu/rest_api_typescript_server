0f63018a63c1b0f4b2352e63ed2b5e34e78760c3
Matiiu <mateotaveraramirez@gmail.com>
2024-04-29 14:31:43 -0500

    add: cors method is added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import colors from 'colors';
+import cors, {CorsOptions} from 'cors';
+import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
 
 import swaggerSpec, {swaggerUiOptions} from './config/swagger';
@@ -22,8 +24,23 @@ connectDB();
 // Installation of Express
 const server = express();
 
+// Allow connections
+const corsOptions: CorsOptions = {
+	origin: (origin, callback) => {
+		if (origin === process.env.FRONT_END_URL) {
+			callback(null, true);
+			return;
+		}
+		callback(new Error('CORS error'));
+	},
+};
+
+server.use(cors(corsOptions));
+
 // Read form data
 server.use(express.json());
+
+server.use(morgan('dev'));
 
 server.use('/api', router);
 
